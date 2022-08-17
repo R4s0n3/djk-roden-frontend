67652bb96b9d868e0dc6bf1328d18151b173bfd4
r40n3 <github@miomideal.com>
2022-08-17 15:46:26 +0200

    launch commit

--- src/team/pages/Teams.js
+++ src/team/pages/Teams.js
@@ -54,11 +54,10 @@ const Teams = props => {
         {props.filter === "Aktive" && <p className="home-content__paragraph">Im Aktivenbereich bieten wir vom Leistungssport, bis hin zum Amateurhandball, eine Plattform für jeden.</p>}
 
         {props.filter === "Jugend" && <p className="home-content__paragraph">
-        Während wir bei unseren Breitensportmannschaften verstärkt den Fokus auf Gemeinschaftserlebnisse und den Spaß an Bewegung und unserem Sport legen, konzentrieren wir uns bei unseren Leistungsmannschaften auf die individuelle Entwicklung der Spieler, gemeinsame sportliche Erfolge und die Leistungsmaximierung der Teams. 
-        <br />
-        Entsprechend spielen wir im männl. Jugendbereich seit vielen Jahren in allen Altersgruppen mit mindestens einer Jugendmannschaft in der höchstmöglichen Spielklasse. Den größten Zuwachs erleben wir aktuell jedoch im weibl. Bereich, sodass wir zunehmend reine Mädchenmannschaften aufbauen können.
-        <br />
-        Aber auch allen Aktiven bieten wir vom Leistungssport, bis hin zum Amateurhandball, eine Plattform. Und schaffen am Ende mit Hilfe fleißiger Trainer- und Betreuerteams regelmäßig neue Erfolgserlebnisse. 
+        Während wir bei unseren Breitensportmannschaften verstärkt den Fokus auf Gemeinschaftserlebnisse und den Spaß an Bewegung und unserem Sport legen, konzentrieren wir uns bei unseren Leistungsmannschaften auf die individuelle Entwicklung der Spieler, gemeinsame sportliche Erfolge und die Leistungsmaximierung der Teams.
+        <br /><br />
+        Entsprechend spielen wir im männl. Jugendbereich seit vielen Jahren in allen Altersgruppen mit mindestens einer Jugendmannschaft in der höchstmöglichen Spielklasse. Aber auch im weiblichen Bereich erleben wir aktuell einen enormen Zuwachs, sodass wir zunehmend reine Mädchenmannschaften aufbauen können.
+
         </p>}
         <div>
             <TeamGrid items={loadedTeams.sort((a, b) => a.index - b.index)} filters={teamFilters} />
